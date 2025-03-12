3719b3af24afd5b43dc90055153f90404075e7f0
JohnMP <j_ponce3@u.pacific.edu>
2025-03-12 15:41:08 -0700

    Initial Commit

---
+++ SmartCaneApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartCaneApp
+//
+//  Created by John Milton Ponce on 3/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartCaneApp/SmartCaneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCaneAppApp.swift
+//  SmartCaneApp
+//
+//  Created by John Milton Ponce on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartCaneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
